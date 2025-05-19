4e669a12e0439f6b144a7743bbfc1bf81608cf60
sharyutilekar <sharyutilekar372@gmail.com>
2025-05-20 01:26:51 +0530

    Initial commit with full COVID-19 dashboard project

---
+++ build/precache-manifest.eaad682284aed277824ce2ec3cc8733c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da5e0d9c9da4397992552a1eea2abe7e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3856becedab4c4125113",
+    "url": "/static/css/main.baa6b235.chunk.css"
+  },
+  {
+    "revision": "b0609a7096664f34c650",
+    "url": "/static/js/2.2dd0416c.chunk.js"
+  },
+  {
+    "revision": "3856becedab4c4125113",
+    "url": "/static/js/main.56e7f673.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
